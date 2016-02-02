2aa0a6351485ad1a34033bc56bc6a494d2c06067
song <zhangsong@corp.1jiajie.com>
2016-02-02 17:36:13 +0800

    init migrate

---
+++ database/migrations/2016_02_02_091626_create_times_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('config_id');
+            $table->integer('type');
+            $table->string('begin');
+            $table->string('end');
+            $table->string('point');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('times');
+    }
+}
---
+++ database/migrations/2016_02_02_093206_create_calls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('config_id');
+            $table->integer('type');
+            $table->integer('order');
+            $table->integer('jump');
+            $table->string('opt');
+            $table->integer('area');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('calls');
+    }
+}
